a39554e38538c18e769f641bedc3626b75740487
daj0g <daj0g@posteo.me>
2026-01-25 21:22:26 +0100

    Magma instrumentation (cross-compiled) added

---
+++ magma/build_cross.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env MAGMA: path to Magma support files
+# - env OUT: path to directory where artifacts are stored
+# - env SHARED: path to directory shared with host (to store results)
+
+# FIX: Guard canary.h against Assembly files BEFORE we build anything.
+# This prevents the "bad instruction" error later in libpng
+sed -i '1i#ifndef __ASSEMBLER__' ${MAGMA}/src/canary.h
+echo "#endif" >> ${MAGMA}/src/canary.h
+
+MAGMA_STORAGE="${SHARED}/canaries.raw" && \
+
+${CC} ${CFLAGS} -D"MAGMA_STORAGE=\"${MAGMA_STORAGE}\"" -c "${MAGMA}/src/canary.c" \
+    -fPIC -I "${MAGMA}/src/" -o "${OUT}/canary.o"
+
+${CC} ${CFLAGS} -D"MAGMA_STORAGE=\"${MAGMA_STORAGE}\"" -c "${MAGMA}/src/storage.c" \
+    -fPIC -I "${MAGMA}/src/" -o "${OUT}/storage.o"
+
+${LD} -r "${OUT}/canary.o" "${OUT}/storage.o" -o "${OUT}/magma.o"
+rm "${OUT}/canary.o" "${OUT}/storage.o"
